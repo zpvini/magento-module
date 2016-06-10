1e55306a23e88dd5fce8cfa5b5bbd65b28f3bac9
Ruan Azevedo <razevedo@mundipagg.com>
2016-06-10 09:41:49 -0300

    :heavy_plus_sign: Adiciona o anti-fraude Stone

--- app/code/community/Uecommerce/Mundipagg/Model/Source/Antifraud.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Source/Antifraud.php
@@ -5,10 +5,12 @@ class Uecommerce_Mundipagg_Model_Source_Antifraud {
 	const ANTIFRAUD_NONE = 0;
 	const ANTIFRAUD_CLEARSALE = 1;
 	const ANTIFRAUD_FCONTROL  = 2;
+	const ANTIFRAUD_STONE  = 3;
 
 	public function toOptionArray() {
 		return array(
 			array('value' => self::ANTIFRAUD_NONE, 'label' => 'Selecione...'),
+			array('value' => self::ANTIFRAUD_STONE, 'label' => 'Stone'),
 			array('value' => self::ANTIFRAUD_CLEARSALE, 'label' => 'Clearsale'),
 			array('value' => self::ANTIFRAUD_FCONTROL, 'label' => 'FControl'),
 		);
---
+++ app/code/community/Uecommerce/Mundipagg/controllers/StoneController.php
@@ -0,0 +1,28 @@
+<?php
+
+class Uecommerce_Mundipagg_StoneController extends Uecommerce_Mundipagg_Controller_Abstract {
+
+	public function getConfigAction() {
+
+		if ($this->requestIsValid() == false) {
+			echo $this->getResponseForInvalidRequest();
+			return false;
+		}
+
+		$entityCode = Mage::getStoreConfig('payment/mundipagg_standard/stone_entitycode');
+		$app = Mage::getStoreConfig('payment/mundipagg_standard/stone_app');
+		$response = array(
+			'entityCode'  => $entityCode,
+			'app'       => $app,
+			'sessionId' => $this->getSessionId()
+		);
+
+		try {
+			return $this->jsonResponse($response);
+
+		} catch (Exception $e) {
+
+		}
+	}
+
+}
