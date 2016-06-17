e155e8c64414b44982580f76e6eab2240a04035d
Ruan Azevedo <razevedo@mundipagg.com>
2016-06-17 15:20:52 -0300

    Insere migration e traducao do campo de ambiente de fingerprint da FControl

---
+++ app/code/community/Uecommerce/Mundipagg/sql/mundipagg_setup/mysql4-upgrade-2.5.7-2.5.8.php
@@ -0,0 +1,11 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+
+Mage::getConfig()->saveConfig('payment/mundipagg_standard/environment_fcontrol', 1);
+Mage::getConfig()->reinit();
+Mage::getConfig()->cleanCache();
+Mage::app()->reinitStores();
+
+$installer->endSetup();
+    
