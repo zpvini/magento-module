403bf63de1f6770959d9414dcfcaa65df624b528
Ruan Azevedo <ruan.azevedo@gmail.com>
2016-08-10 16:09:21 -0300

    :mute: Remove unnecessary validation logs

--- app/code/community/Uecommerce/Mundipagg/Model/Urlvalidation.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Urlvalidation.php
@@ -3,12 +3,9 @@
 class Uecommerce_Mundipagg_Model_Urlvalidation extends Mage_Core_Model_Config_Data {
 
 	public function save() {
-		$logHelper = new Uecommerce_Mundipagg_Helper_Log(__METHOD__);
 		$url = $this->getValue();
 		$parsedUrl = parse_url($url);
 		$path = null;
-
-		$logHelper->info("parsed: " . print_r($parsedUrl, 1));
 
 		if (isset($parsedUrl['path'])) {
 			$path = strtolower($parsedUrl['path']);
@@ -19,8 +16,6 @@ class Uecommerce_Mundipagg_Model_Urlvalidation extends Mage_Core_Model_Config_Da
 		}
 
 		$newUrl = "{$parsedUrl['scheme']}://{$parsedUrl['host']}/{$parsedUrl['path']}";
-
-		$logHelper->info("newUrl: {$newUrl}");
 
 		$this->setValue($newUrl);
 
