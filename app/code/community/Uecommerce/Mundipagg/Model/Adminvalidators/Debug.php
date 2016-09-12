7df65b7bab798fa6eaa1ead5e5eab642529c8320
Ruan Azevedo <ruan.azevedo@gmail.com>
2016-09-12 12:45:04 -0300

    :bug: Debug fix

--- app/code/community/Uecommerce/Mundipagg/Model/Adminvalidators/Debug.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Adminvalidators/Debug.php
@@ -2,27 +2,20 @@
 
 class Uecommerce_Mundipagg_Model_Adminvalidators_Debug extends Mage_Core_Model_Config_Data {
 
-	const INATIVE = 0;
-	const ACTIVE  = 1;
+	const DISABLED = 0;
+	const ENABLED  = 1;
 
 	public function save() {
 		$debug = $this->getValue();
-		$log = new Uecommerce_Mundipagg_Helper_Log(__METHOD__);
-
-		if ($debug == self::INATIVE)
-			return;
-
 		$pathLogConfig = 'dev/log/active';
 		$logIsActive = Mage::getStoreConfig($pathLogConfig);
 
-		if ($logIsActive == self::ACTIVE) //log already active
-			return;
-
-		try {
-			Mage::getConfig()->saveConfig($pathLogConfig, self::ACTIVE);
-
-		} catch (Exception $e) {
-			throw new Exception($e->getMessage());
+		if ($debug == self::ENABLED && $logIsActive == self::DISABLED) {
+			try {
+				Mage::getConfig()->saveConfig($pathLogConfig, self::ENABLED);
+			} catch (Exception $e) {
+				throw new Exception($e->getMessage());
+			}
 		}
 
 		return parent::save();
