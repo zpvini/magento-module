f685e9edf15c481c1a90e9e6ce20122a4572cdd3
Ruan Azevedo <ruan.azevedo@gmail.com>
2016-09-20 16:35:03 -0300

    :bug: Fix for PHP 5.3 and below

--- app/code/community/Uecommerce/Mundipagg/Helper/Util.php
+++ app/code/community/Uecommerce/Mundipagg/Helper/Util.php
@@ -3,7 +3,20 @@
 class Uecommerce_Mundipagg_Helper_Util extends Mage_Core_Helper_Abstract {
 
 	public function jsonEncodePretty($input) {
-		return json_encode($input, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+		$version = phpversion();
+		$version = explode('.', $version);
+		$version = $version[0] . $version[1];
+		$version = intval($version);
+
+		// JSON Variables available only in PHP 5.4
+		if ($version <= 53) {
+			$result = json_encode($input);
+
+		} else {
+			$result = json_encode($input, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+		}
+
+		return $result;
 	}
 
 } 
