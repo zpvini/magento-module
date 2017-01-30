7ac295e793bd6926a5166dee074a15a54a2eb608
Ruan Azevedo <ruan.azevedo@gmail.com>
2017-01-30 19:47:41 -0200

    :art: Admin "Payment Information" refact

--- app/code/community/Uecommerce/Mundipagg/Block/Info.php
+++ app/code/community/Uecommerce/Mundipagg/Block/Info.php
@@ -27,50 +27,95 @@
  * @package    Uecommerce_Mundipagg
  * @author     Uecommerce Dev Team
  */
+class Uecommerce_Mundipagg_Block_Info extends Mage_Payment_Block_Info {
 
-class Uecommerce_Mundipagg_Block_Info extends Mage_Payment_Block_Info
-{
-	protected function _construct()
-	{
+	protected function _construct() {
 		parent::_construct();
 		$this->setTemplate('mundipagg/payment/info/mundipagg.phtml');
 	}
-	
+
 	/**
-     * Retrieve order model instance
-     *
-     * @return Mage_Sales_Model_Order
-     */
-	public function getOrder() 
-    {
+	 * Retrieve order model instance
+	 *
+	 * @return Mage_Sales_Model_Order
+	 */
+	public function getOrder() {
 		return Mage::registry('current_order');
 	}
-	
+
 	/**
-     * Retrieve invoice model instance
-     *
-     * @return Mage_Sales_Model_Order_Invoice
-     */
-	public function getInvoice() 
-    {
+	 * Retrieve invoice model instance
+	 *
+	 * @return Mage_Sales_Model_Order_Invoice
+	 */
+	public function getInvoice() {
 		return Mage::registry('current_invoice');
 	}
-	
+
 	/**
-     * Retrieve shipment model instance
-     *
-     * @return Mage_Sales_Model_Order_Shipment
-     */
-    public function getShipment() 
-    {
-    	return Mage::registry('current_shipment');
-    }
-    
-    /**
-     * Retrieve payment method
-     */
-    public function getFormaPagamento() 
-    {
-    	return $this->getInfo()->getAdditionalInformation('PaymentMethod');
-    }
+	 * Retrieve shipment model instance
+	 *
+	 * @return Mage_Sales_Model_Order_Shipment
+	 */
+	public function getShipment() {
+		return Mage::registry('current_shipment');
+	}
+
+	/**
+	 * Retrieve payment method
+	 */
+	public function getFormaPagamento() {
+		return $this->getInfo()->getAdditionalInformation('PaymentMethod');
+	}
+
+	/**
+	 * @param $ccQty credit card quantity
+	 * @param $ccPos credit card position
+	 * @return array|mixed|null
+	 */
+	public function getCcBrand($ccQty, $ccPos) {
+		if ($ccQty == 1) {
+			$ccBrand = $this->getInfo()
+				->getAdditionalInformation("mundipagg_twocreditcards_{$ccQty}_{$ccPos}__cc_type");
+
+			if (empty($ccBrand)) {
+				$ccBrand = $this->getInfo()
+					->getAdditionalInformation("CreditCardBrandEnum_mundipagg_creditcard_token_{$ccQty}_{$ccPos}");
+			}
+		} else {
+			$ccBrand = $this->getInfo()
+				->getAdditionalInformation("mundipagg_twocreditcards_{$ccQty}_{$ccPos}_cc_type");
+
+			if (empty($ccBrand)) {
+				$ccBrand = $this->getInfo()
+					->getAdditionalInformation("CreditCardBrandEnum_mundipagg_twocreditcards_token_{$ccQty}_{$ccPos}");
+			}
+		}
+
+		return $ccBrand;
+	}
+
+	public function getCcValue($ccQty, $ccPos) {
+		if ($ccQty == 1) {
+			$value = (float)$this->getInfo()->getAdditionalInformation("1_AmountInCents") * 0.01;
+
+		} else {
+			$value = $this->getInfo()->getAdditionalInformation("mundipagg_twocreditcards_value_{$ccQty}_{$ccPos}");
+		}
+
+		return Mage::helper('core')->currency($value, true, false);
+	}
+
+	public function getInstallmentsNumber($ccQty, $ccPos) {
+		if ($ccQty == 1) {
+			$installments = $this->getInfo()->getAdditionalInformation("mundipagg_creditcard_credito_parcelamento_1_1");
+		} else {
+			$installments = $this->getInfo()
+				->getAdditionalInformation("mundipagg_twocreditcards_credito_parcelamento_{$ccQty}_{$ccPos}");
+		}
+		$installments .= "x";
+
+		return $installments;
+	}
+
 }
