77282c202f7c88aeeeaed4f0f494141a0be6b7d3
michelpl <michelpl@yahoo.com.br>
2017-07-21 18:04:42 -0300

    :art: Add recurrence as a payment method.

--- app/code/community/Uecommerce/Mundipagg/Model/Observer.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Observer.php
@@ -1,33 +1,5 @@
 <?php
 
-/**
- * Uecommerce
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Uecommerce EULA.
- * It is also available through the world-wide-web at this URL:
- * http://www.uecommerce.com.br/
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade the extension
- * to newer versions in the future. If you wish to customize the extension
- * for your needs please refer to http://www.uecommerce.com.br/ for more information
- *
- * @category   Uecommerce
- * @package    Uecommerce_Mundipagg
- * @copyright  Copyright (c) 2012 Uecommerce (http://www.uecommerce.com.br/)
- * @license    http://www.uecommerce.com.br/
- */
-
-/**
- * Mundipagg Payment module
- *
- * @category   Uecommerce
- * @package    Uecommerce_Mundipagg
- * @author     Uecommerce Dev Team
- */
 class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Standard {
 	/*
 	 * Update status and notify customer or not
@@ -206,6 +178,7 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
 					$result->isAvailable = false;
 					break;
 				case 'mundipagg_creditcard':
+                case 'mundipagg_recurrencepayment':
 					$result->isAvailable = true;
 					break;
 				default:
