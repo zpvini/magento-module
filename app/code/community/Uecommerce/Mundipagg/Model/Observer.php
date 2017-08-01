86b09d270477fe9ca489f459f5fd5f5cca8b45bf
michelpl <michelpl@yahoo.com.br>
2017-08-01 19:31:21 -0300

    :heavy_plus_sign: Show conflict messages.

--- app/code/community/Uecommerce/Mundipagg/Model/Observer.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Observer.php
@@ -156,7 +156,8 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
 	}
 
 	/**
-	 * Check if recurrency product is in cart in order to show only Mundipagg Credit Card payment
+	 * Check if recurrency product is in cart in order to show
+     * only Mundipagg Credit Card payment
 	 */
 	public function checkForRecurrency($observer) {
 		$session = Mage::getSingleton('checkout/session');
@@ -221,6 +222,25 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
         }
     }
 
+    public function cartCheckRecurrencyConflicts($observer)
+    {
+        $recurrencePayment = Mage::getStoreConfig('payment/mundipagg_recurrencepayment/active');
+        if ($recurrencePayment === '1') {
+
+            $msgConflictRecurrentOthers = Mage::getStoreConfig('payment/mundipagg_recurrencepayment/conflict_message_recurrent_others');
+            $msgConflictRecurrentMixMix = Mage::getStoreConfig('payment/mundipagg_recurrencepayment/conflict_message_recurrent_mix_mix');
+            $msgConflictRecurrentMixRecurrent = Mage::getStoreConfig('payment/mundipagg_recurrencepayment/conflict_message_recurrent_mix_recurrent');
+
+            Mage::getSingleton('checkout/session')->addError($msgConflictRecurrentOthers);
+            Mage::getSingleton('checkout/session')->addError($msgConflictRecurrentMixMix);
+            Mage::getSingleton('checkout/session')->addError($msgConflictRecurrentMixRecurrent);
+            Mage::getSingleton('checkout/session')->addError($this->__(''));
+            return false;
+            
+        }
+    }
+
+
 	/**
 	 * Add discount amount in the quote when partial payment
 	 *
@@ -230,7 +250,11 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
 		$session = Mage::getSingleton('checkout/session');
 		if (!$session->getApprovalRequestSuccess() == 'partial') {
 			$request = Mage::app()->getRequest();
-			if (Mage::app()->getRequest()->getActionName() != 'partialPost' && $request->getModuleName() != 'mundipagg' && $request->getControllerName() != 'standard') {
+			if (
+                Mage::app()->getRequest()->getActionName() != 'partialPost' &&
+                $request->getModuleName() != 'mundipagg' &&
+                $request->getControllerName() != 'standard'
+                ) {
 				return $this;
 			}
 		}
@@ -420,63 +444,63 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
 		$session->setMundipaggRecurrency($option);
 	}
         
-        public function checkModuleVersion()
-        {
-            $localModuleVersion = $this->readModuleVersion();
-            $repoVersion = $this->getRepoVersion();
-            if(version_compare($localModuleVersion, $repoVersion, "<") == 1){
-                $this->insertOldVersionNotification($localModuleVersion, $repoVersion);
-            }
+    public function checkModuleVersion()
+    {
+        $localModuleVersion = $this->readModuleVersion();
+        $repoVersion = $this->getRepoVersion();
+        if(version_compare($localModuleVersion, $repoVersion, "<") == 1){
+            $this->insertOldVersionNotification($localModuleVersion, $repoVersion);
         }
-        
-        private function insertOldVersionNotification($oldVersion, $newVersion){
-            $notification = mage::getModel("adminnotification/inbox");
-            $data = [
-                'severity'=>Mage_AdminNotification_Model_Inbox::SEVERITY_MINOR
-                ,'title'=> 'Nova versão do módulo de integração Mundipagg disponível.'
-                ,'description'=> 
-                    "Você está utilizando uma versão antiga do módulo de interação Mundipagg(v" . $oldVersion . "). Atualize para a versão v" . $newVersion . "<br>
-                    <a href='https://www.magentocommerce.com/magento-connect/mundipagg-payment-gateway.html' target='_blank'>Download Magento Connect</a><br>
-                    <a href='https://github.com/mundipagg/Magento.Integracao' target='_blank'>GitHub</a>
-                    "
-                ,'url' =>"https://www.magentocommerce.com/magento-connect/mundipagg-payment-gateway.html"
-                ,'is_read' => 0
-                ,'is_remove'=>0
-                ,'date_added'=> now()
-            ];
-            $notification->setData($data);
-            $notification->save();
+    }
+
+    private function insertOldVersionNotification($oldVersion, $newVersion){
+        $notification = mage::getModel("adminnotification/inbox");
+        $data = [
+            'severity'=>Mage_AdminNotification_Model_Inbox::SEVERITY_MINOR
+            ,'title'=> 'Nova versão do módulo de integração Mundipagg disponível.'
+            ,'description'=> 
+                "Você está utilizando uma versão antiga do módulo de interação Mundipagg(v" . $oldVersion . "). Atualize para a versão v" . $newVersion . "<br>
+                <a href='https://www.magentocommerce.com/magento-connect/mundipagg-payment-gateway.html' target='_blank'>Download Magento Connect</a><br>
+                <a href='https://github.com/mundipagg/Magento.Integracao' target='_blank'>GitHub</a>
+                "
+            ,'url' =>"https://www.magentocommerce.com/magento-connect/mundipagg-payment-gateway.html"
+            ,'is_read' => 0
+            ,'is_remove'=>0
+            ,'date_added'=> now()
+        ];
+        $notification->setData($data);
+        $notification->save();
+    }
+
+    private function readModuleVersion(){
+        $configXml = Mage::getBaseDir('app') . "/code/community/Uecommerce/Mundipagg/etc/config.xml";
+        if (file_exists($configXml)) {
+            $xmlObj = simplexml_load_file($configXml);
+            return $xmlObj->modules->Uecommerce_Mundipagg->version[0];
+        }else{
+            return 0;
         }
+    }
 
-        private function readModuleVersion(){
-            $configXml = Mage::getBaseDir('app') . "/code/community/Uecommerce/Mundipagg/etc/config.xml";
-            if (file_exists($configXml)) {
-                $xmlObj = simplexml_load_file($configXml);
-                return $xmlObj->modules->Uecommerce_Mundipagg->version[0];
-            }else{
-                return 0;
-            }
+    private function getRepoVersion(){
+        $url = "https://api.github.com/repos/mundipagg/Magento.Integracao/releases";
+        $ch = curl_init();
+
+        // Header
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+        curl_setopt($ch,CURLOPT_USERAGENT,'Mundipagg');
+        // Set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // Execute post
+        $_response = curl_exec($ch);
+
+        if (curl_errno($ch)) {
+            $helperLog->info(curl_error($ch));
         }
+        $response = (json_decode($_response));
+        return str_replace("v", "",$response[0]->tag_name);
 
-        private function getRepoVersion(){
-            $url = "https://api.github.com/repos/mundipagg/Magento.Integracao/releases";
-            $ch = curl_init();
-
-            // Header
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-            curl_setopt($ch,CURLOPT_USERAGENT,'Mundipagg');
-            // Set the url, number of POST vars, POST data
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            // Execute post
-            $_response = curl_exec($ch);
-
-            if (curl_errno($ch)) {
-                $helperLog->info(curl_error($ch));
-            }
-            $response = (json_decode($_response));
-            return str_replace("v", "",$response[0]->tag_name);
-
-        }
+    }
 
 }
