c5c9f512fc8a6a617d6d538d6eeb710498795fe9
michelpl <michelpl@yahoo.com.br>
2017-08-11 19:28:18 -0300

    :bug: Fix credit card brand selection.

--- app/code/community/Uecommerce/Mundipagg/Model/Api.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Api.php
@@ -1,32 +1,5 @@
 <?php
-/**
- * Uecommerce
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Uecommerce EULA.
- * It is also available through the world-wide-web at this URL:
- * http://www.uecommerce.com.br/
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade the extension
- * to newer versions in the future. If you wish to customize the extension
- * for your needs please refer to http://www.uecommerce.com.br/ for more information
- *
- * @category   Uecommerce
- * @package    Uecommerce_Mundipagg
- * @copyright  Copyright (c) 2012 Uecommerce (http://www.uecommerce.com.br/)
- * @license    http://www.uecommerce.com.br/
- */
 
-/**
- * Mundipagg Payment module
- *
- * @category   Uecommerce
- * @package    Uecommerce_Mundipagg
- * @author     Uecommerce Dev Team
- */
 class Uecommerce_Mundipagg_Model_Api extends Uecommerce_Mundipagg_Model_Standard {
 
 	const TRANSACTION_NOT_FOUND        = "Transaction not found";
@@ -41,6 +14,15 @@ class Uecommerce_Mundipagg_Model_Api extends Uecommerce_Mundipagg_Model_Standard
 	private $modelStandard;
 	private $debugEnabled;
 	private $moduleVersion;
+
+    private $creditCardBrands = [
+      'VI' => 'Visa',
+      'MC' => 'Mastercard',
+      'AE' => 'Amex',
+      'DI' => 'Diners',
+      'EL' => 'Elo',
+      'HI' => 'Hipercard'
+    ];
 
 	public function __construct($Store = null) {
 		$this->helperUtil = new Uecommerce_Mundipagg_Helper_Util();
@@ -107,7 +89,7 @@ class Uecommerce_Mundipagg_Model_Api extends Uecommerce_Mundipagg_Model_Standard
 
 					if ($token->getId() && $token->getEntityId() == $order->getCustomerId()) {
 						$creditcardTransactionData->CreditCard->InstantBuyKey = $token->getToken();
-						$creditcardTransactionData->CreditCard->CreditCardBrand = $token->getCcType();
+						$creditcardTransactionData->CreditCard->CreditCardBrand = $this->creditCardBrands[$token->getCcType()];
 						/** Tipo de operação: AuthOnly | AuthAndCapture | AuthAndCaptureWithDelay  */
 						$creditcardTransactionData->CreditCardOperation = $creditCardOperationEnum;
 						$creditcardTransactionData->AmountInCents = intval(strval(($paymentData['AmountInCents']))); // Valor da transação
@@ -121,7 +103,7 @@ class Uecommerce_Mundipagg_Model_Api extends Uecommerce_Mundipagg_Model_Standard
 					$creditcardTransactionData->CreditCard->SecurityCode = $paymentData['SecurityCode']; // Código de segurança
 					$creditcardTransactionData->CreditCard->ExpMonth = $paymentData['ExpMonth']; // Mês Exp
 					$creditcardTransactionData->CreditCard->ExpYear = $paymentData['ExpYear']; // Ano Exp 
-					$creditcardTransactionData->CreditCard->CreditCardBrand = $paymentData['CreditCardBrandEnum']; // Bandeira do cartão : Visa ,MasterCard ,Hipercard ,Amex */
+					$creditcardTransactionData->CreditCard->CreditCardBrand = $this->creditCardBrands[$paymentData['CreditCardBrandEnum']]; // Bandeira
 					$creditcardTransactionData->CreditCardOperation = $creditCardOperationEnum;
 					/** Tipo de operação: AuthOnly | AuthAndCapture | AuthAndCaptureWithDelay  */
 					$creditcardTransactionData->AmountInCents = intval(strval(($paymentData['AmountInCents']))); // Valor da transação
