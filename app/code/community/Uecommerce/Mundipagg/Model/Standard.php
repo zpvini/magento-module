e8f322487a33bef848d62236c677eea77c48c4ce
Michel Lima <michelpl@yahoo.com.br>
2017-08-17 18:12:04 -0300

    :bug:Fix amount comparison.

--- app/code/community/Uecommerce/Mundipagg/Model/Standard.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Standard.php
@@ -613,7 +613,7 @@ class Uecommerce_Mundipagg_Model_Standard extends Mage_Payment_Model_Method_Abst
                         count($ccResultCollection) > 1 && $this->getAntiFraud() == 0 && $this->getPaymentAction() == 'order' && $accPaymentAuthorizationAmount == $accGrandTotal
                 ) {
                     $this->captureAndcreateInvoice($payment);
-                } else if ($accPaymentAuthorizationAmount != $accGrandTotal) {
+                } else if ($accPaymentAuthorizationAmount < $accGrandTotal) {
                     $order->cancel();
                     $order->setState(Mage_Sales_Model_Order::STATE_CANCELED, true)->save();
                     $order->setStatus(Mage_Sales_Model_Order::STATE_CANCELED);
