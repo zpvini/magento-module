276f1cea8305c8d7896992c7a879416314207918
Michel Lima <michelpl@yahoo.com.br>
2017-09-06 17:02:29 -0300

    :construction: Fix recurrence payment.

--- app/code/community/Uecommerce/Mundipagg/Model/Observer.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Observer.php
@@ -545,18 +545,30 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
             }
         }
     }
+    
+    private function getCouponDiscount($items)
+    {
+        $discountAmount = 0;
+        foreach ($items as $item){
+            $discountAmount += $item->getDiscountAmount();
+        }
+        
+        return $discountAmount;
+    }
 
-    public function changeRecurrenceValues(Varien_Event_Observer $observer) {
-        /*$quote = Mage::getSingleton('checkout/session')->getQuote();
+    public function changeRecurrenceValues(Varien_Event_Observer $observer)
+    {       
+        $quote = Mage::getSingleton('checkout/session')->getQuote();
         $quoteid = $quote->getId();
-
-        $payment = $quote->getPayment();
-
-        $quoteItems = $quote->getAllItems();
-        $discountAmount = $this->getRecurrenceDiscount($quoteItems);
-
+      
+        $couponDiscount = $this->getCouponDiscount($quote->getAllItems());
+        $interest = $quote->getMundipaggInterest();
+        $paymentMethod = $quote->getPayment()->getMethod();
+        
         if ($quoteid) {
             $total = $quote->getGrandTotal();
+            $total = $total - $couponDiscount;
+            $total = $total + $interest;
 
             $quote->setSubtotal(0);
             $quote->setBaseSubtotal(0);
@@ -567,10 +579,9 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
             $quote->setGrandTotal(0);
             $quote->setBaseGrandTotal(0);
 
-
             $canAddItems = $quote->isVirtual() ? ('billing') : ('shipping');
+            
             foreach ($quote->getAllAddresses() as $address) {
-
                 $address->setSubtotal(0);
                 $address->setBaseSubtotal(0);
 
@@ -579,71 +590,56 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
 
                 $address->collectTotals();
 
-                $quote->setSubtotal((float) $quote->getSubtotal() + $address->getSubtotal());
-                $quote->setBaseSubtotal((float) $quote->getBaseSubtotal() + $address->getBaseSubtotal());
+                $quote->setSubtotal((float) $total);
+                $quote->setBaseSubtotal((float) $total);
 
-                $quote->setGrandTotal((float) $quote->getGrandTotal() + $address->getGrandTotal());
-                $quote->setBaseGrandTotal((float) $quote->getBaseGrandTotal() + $address->getBaseGrandTotal());
+                $quote->setGrandTotal((float) $total);
+                $quote->setBaseGrandTotal((float) $total);
 
                 $quote->save();
-
-                $quote->setGrandTotal($quote->getBaseSubtotal() - $discountAmount)
-                        ->setBaseGrandTotal($quote->getBaseSubtotal() - $discountAmount)
-                        ->setSubtotalWithDiscount($quote->getBaseSubtotal() - $discountAmount)
-                        ->setBaseSubtotalWithDiscount($quote->getBaseSubtotal() - $discountAmount)
-                        ->save();
-
-                if (
-                    $this->checkRecurrenceMix($quote) &&
-                    $this->checkItemAlone($quote)
-                ) {
-                    $paymentMethod = $quote->getPayment()->getMethod();
-                    if (
-                        $paymentMethod === 'mundipagg_recurrencepayment'
-                    ) {
-                        $totalWithDiscount = $this->getRecurrencePartial($total, $quoteItems);
+                
+                if ($this->checkRecurrenceMix($quote) && $this->checkItemAlone($quote)) {
+                    // pagamento recorrente
+                    if ($paymentMethod === 'mundipagg_recurrencepayment') {
+                        $recurrenceInstallmentValue = $this->getRecurrencePartial(
+                            $quote->getGrandTotal(),
+                            $quoteItems
+                        );
+                        
                         if ($address->getAddressType() == $canAddItems) {
-                            $msg = Mage::getStoreConfig('payment/mundipagg_recurrencepayment/recurrent_mix_message');
-                            $address->setSubtotalWithDiscount((float) $totalWithDiscount);
-                            $address->setGrandTotal((float) $totalWithDiscount);
-                            $address->setBaseSubtotalWithDiscount((float) $totalWithDiscount);
-                            $address->setBaseGrandTotal((float) $totalWithDiscount);
-                            if ($address->getDiscountDescription()) {
-                                $address->setDiscountAmount(-($address->getDiscountAmount()));
-                                $address->setDiscountDescription(
-                                    $msg
-                                );
-                                $address->setBaseDiscountAmount($address->getBaseDiscountAmount());
-                            } else {
-                                $address->setDiscountAmount($totalWithDiscount);
-                                $address->setDiscountDescription($msg);
-                            }
+                            $address->setSubtotalWithDiscount((float) $recurrenceInstallmentValue);
+                            $address->setGrandTotal((float) $recurrenceInstallmentValue);
+                            $address->setBaseSubtotalWithDiscount((float) $recurrenceInstallmentValue);
+                            $address->setBaseGrandTotal((float) $recurrenceInstallmentValue);
+                            
                             $address->save();
                         }
+                    // pagamento avulso
                     } else {
                         if ($address->getAddressType() == $canAddItems) {
-                            $msg = 'Desconto para pagamento avulso';
-                            $address->setSubtotalWithDiscount((float) $address->getSubtotalWithDiscount() - $discountAmount);
-                            $address->setGrandTotal((float) $address->getGrandTotal() - $discountAmount);
-                            $address->setBaseSubtotalWithDiscount((float) $address->getBaseSubtotalWithDiscount() - $discountAmount);
-                            $address->setBaseGrandTotal((float) $address->getBaseGrandTotal() - $discountAmount);
-                            if ($address->getDiscountDescription()) {
-                                $address->setDiscountAmount(-($address->getDiscountAmount() - $discountAmount));
-                                $address->setDiscountDescription(
-                                        $address->getDiscountDescription() . $msg
-                                );
-                                $address->setBaseDiscountAmount(-($address->getBaseDiscountAmount() - $discountAmount));
-                            } else {
-                                $address->setDiscountAmount(-($discountAmount));
-                                $address->setDiscountDescription($msg);
-                                $address->setBaseDiscountAmount(-($discountAmount));
-                            }
+                            $msg = 'Desconto para pagamento único';
+                            
+                            $total -= $this->getRecurrenceDiscount($quoteItems);
+                            $discountAmount = $this->getRecurrenceDiscount($quoteItems);
+                            $subtotalWithDiscount = (float) $total - $discountAmount;
+                            $grandTotal = (float) $total;
+                            $baseSubtotalWithDiscount = (float) $total - $discountAmount;
+                            $baseGrandTotal = 100;//(float) $address->getBaseGrandTotal() - $discountAmount;
+                            
+                            $address->setSubtotalWithDiscount($subtotalWithDiscount);
+                            $address->setGrandTotal($grandTotal);
+                            $address->setBaseSubtotalWithDiscount($baseSubtotalWithDiscount);
+                            $address->setBaseGrandTotal($baseGrandTotal);
+                            
+                            $address->setDiscountAmount($discountAmount * (-1));
+                            $address->setDiscountDescription($address->getDiscountDescription() . " | " .$msg);
+                            $address->setBaseDiscountAmount($discountAmount * (-1));
                             $address->save();
                         }
                     }
                 }
             }
-        }*/
+        }
     }
 
     private function getRecurrencePartial($total, $items)
@@ -673,15 +669,17 @@ class Uecommerce_Mundipagg_Model_Observer extends Uecommerce_Mundipagg_Model_Sta
             }
     }
 
-    private function getRecurrenceDiscount( $items)
+    private function getRecurrenceDiscount($items)
     {
         foreach($items as $item) {
-                $product = $item->getProduct();
-                $discount = $product->getMundipaggRecurrenceDiscount();
-                if ($discount > 0) {
-                    return $discount;
-                }
+            $product = $item->getProduct();
+            $discount = $product->getMundipaggRecurrenceDiscount();
+            $price = $product->getPrice();
+            if ($discount > 0) {
+                $desconto = $price * ($discount/100);
+                return $desconto;
             }
+        }
     }
 
     public function catalogProductSaveBefore($event) {
