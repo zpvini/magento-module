c88303c48ad0883d20a3af9facce81d4f328e851
felipebool <bolzin@gmail.com>
2018-03-28 12:03:41 -0300

    :bug: fix 2 credit card balance with magento theme

--- app/code/community/Uecommerce/Mundipagg/Model/Creditcard.php
+++ app/code/community/Uecommerce/Mundipagg/Model/Creditcard.php
@@ -21,7 +21,7 @@ class Uecommerce_Mundipagg_Model_Creditcard extends Uecommerce_Mundipagg_Model_S
     protected $_canSaveCc = false;
     protected $_canFetchTransactionInfo = false;
     protected $_canManageRecurringProfiles = false;
-    protected $_allowCurrencyCode = array('BRL', 'USD', 'EUR');
+    protected $_allowCurrencyCode = ['BRL', 'USD', 'EUR'];
     protected $_isInitializeNeeded = true;
 
     /**
@@ -29,13 +29,19 @@ class Uecommerce_Mundipagg_Model_Creditcard extends Uecommerce_Mundipagg_Model_S
      * @access public
      * @param array $data
      * @return Uecommerce_Mundipagg_Model_Standard
+     * @throws Varien_Exception
      */
     public function assignData($data)
     {
-        if (isset($data[$this->_code.'_token_1_1']) && $data[$this->_code.'_token_1_1'] != 'new') {
+        if (
+            isset($data[$this->_code.'_token_1_1']) &&
+            $data[$this->_code.'_token_1_1'] != 'new'
+        ) {
             $parcelsNumber = $data[$this->_code.'_credito_parcelamento_1_1'];
-            $cardonFile = Mage::getModel('mundipagg/cardonfile')->load($data[$this->_code.'_token_1_1']);
-            $cctype = Mage::getSingleton('mundipagg/source_cctypes')->getCcTypeForLabel($cardonFile->getCcType());
+            $cardonFile = Mage::getModel('mundipagg/cardonfile')
+                ->load($data[$this->_code.'_token_1_1']);
+            $cctype = Mage::getSingleton('mundipagg/source_cctypes')
+                ->getCcTypeForLabel($cardonFile->getCcType());
         } else {
             $cctype = $data[$this->_code.'_1_1_cc_type'];
             $parcelsNumber = $data[$this->_code.'_new_credito_parcelamento_1_1'];
@@ -45,24 +51,47 @@ class Uecommerce_Mundipagg_Model_Creditcard extends Uecommerce_Mundipagg_Model_S
         $info->getQuote()->setTotalsCollectedFlag(false)->collectTotals();
         $info = $this->resetInterest($info);
 
-        $discount = Uecommerce_Mundipagg_Helper_Installments::getRecurrenceDiscount($info->getQuote());
-        $interest = Mage::helper('mundipagg/installments')->getInterestForCard($parcelsNumber, $cctype, $info->getQuote()->getGrandTotal() - $discount);
+        $discount = Uecommerce_Mundipagg_Helper_Installments::getRecurrenceDiscount(
+            $info->getQuote()
+        );
+        $interest = Mage::helper('mundipagg/installments')
+            ->getInterestForCard(
+                $parcelsNumber,
+                $cctype,
+                $info->getQuote()->getGrandTotal() - $discount
+            );
 
         if ($interest > 0) {
-            $interestInformation = array();
+            $interestInformation = [];
             $interestInformation[$this->_code.'_1_1'] = new Varien_Object();
-            $interestInformation[$this->_code.'_1_1']->setInterest(str_replace(',', '.', $interest));
-            $info->setAdditionalInformation('mundipagg_interest_information', array());
-            $info->setAdditionalInformation('mundipagg_interest_information', $interestInformation);
+            $interestInformation[$this->_code.'_1_1']
+                ->setInterest(str_replace(
+                    ',',
+                    '.',
+                    $interest)
+                );
+            $info->setAdditionalInformation(
+                'mundipagg_interest_information',
+                []
+            );
+
+            $info->setAdditionalInformation(
+                'mundipagg_interest_information',
+                $interestInformation
+            );
             $this->applyInterest($info, $interest);
         } else {
             // If none of Cc parcels doens't have interest we reset interest
             $info = $this->resetInterest($info);
         }
-        $discount = Uecommerce_Mundipagg_Helper_Installments::getRecurrenceDiscount($info->getQuote());
+
+        $discount = Uecommerce_Mundipagg_Helper_Installments::getRecurrenceDiscount(
+            $info->getQuote()
+        );
+
         foreach ($info->getQuote()->getAllAddresses() as $address) {
             $grandTotal = $address->getGrandTotal();
-            $messages = array();
+            $messages = [];
             if ($address->getDiscountDescription()) {
                 $messages[] = $address->getDiscountDescription();
             }
@@ -70,8 +99,12 @@ class Uecommerce_Mundipagg_Model_Creditcard extends Uecommerce_Mundipagg_Model_S
                 $address->setMundipaggInterest($interest);
                 $address->setGrandTotal($grandTotal - $discount + $interest);
                 if ($discount) {
-                    $address->setDiscountAmount(($address->getDiscountAmount() - $discount));
-                    $msgRecurrence = Mage::getStoreConfig('payment/mundipagg_recurrencepayment/recurrence_discount_message');
+                    $address->setDiscountAmount(
+                        ($address->getDiscountAmount() - $discount)
+                    );
+                    $msgRecurrence = Mage::getStoreConfig(
+                        'payment/mundipagg_recurrencepayment/recurrence_discount_message'
+                    );
                     if ($msgRecurrence) {
                         $messages[] = $msgRecurrence;
                     }
@@ -99,8 +132,8 @@ class Uecommerce_Mundipagg_Model_Creditcard extends Uecommerce_Mundipagg_Model_S
     /**
      * Instantiate state and set it to state object
      *
-     * @param string $paymentAction
-     * @param Varien_Object
+     * @param $paymentAction
+     * @param $stateObject
      * @throws Varien_Exception
      */
     public function initialize($paymentAction, $stateObject)
@@ -116,6 +149,10 @@ class Uecommerce_Mundipagg_Model_Creditcard extends Uecommerce_Mundipagg_Model_S
         $baseAmountOrder = $payment->getBaseAmountOrdered();
         $antiFraudEnabled = $this->getAntiFraud();
 
+        $helperLog = new Uecommerce_Mundipagg_Helper_Log(__METHOD__);
+        $helperLog->setLogLabel('|' . $order->getIncrementId() . '|');
+        $helperLog->info('initialize()');
+
         if (
             $antiFraudEnabled &&
             $standard->getConfigData('payment_action') == 'order' &&
@@ -126,24 +163,32 @@ class Uecommerce_Mundipagg_Model_Creditcard extends Uecommerce_Mundipagg_Model_S
 
         switch ($paymentAction) {
             case 'order':
-                $this->setCreditCardOperationEnum('AuthAndCapture');
+                $this->setCreditCardOperationEnum(
+                    'AuthAndCapture'
+                );
 
                 $paymentAction = $orderAction = 'order';
                 break;
 
             case 'authorize':
-                $this->setCreditCardOperationEnum('AuthOnly');
+                $this->setCreditCardOperationEnum(
+                    'AuthOnly'
+                );
 
                 $paymentAction = $orderAction = 'authorize';
                 break;
 
             case 'authorize_capture':
-                $this->setCreditCardOperationEnum('AuthAndCaptureWithDelay');
+                $this->setCreditCardOperationEnum(
+                    'AuthAndCaptureWithDelay'
+                );
 
                 $paymentAction = $orderAction = 'authorize_capture';
                 break;
         }
 
+        $helperLog->info('Payment method: CreditCard');
+        $helperLog->info('Payment action: ' . $paymentAction);
 
         switch ($paymentAction) {
             case Mage_Payment_Model_Method_Abstract::ACTION_AUTHORIZE:
