a7e1a6e65a4fdbb8013e401d65f68ab404efb6cd
Ian Lessa <ian.lessa1@gmail.com>
2018-06-21 17:03:55 -0300

    :green_heart: added module sql configs

--- .circleci/data/magento_one_module_install.sql
+++ .circleci/data/magento_one_module_install.sql
@@ -1,53 +1,20 @@
-INSERT INTO magento.core_config_data (config_id, scope, scope_id, path, value)
-VALUES (NULL , 'default', '0', 'dev/template/allow_symlink', '1');
-
 INSERT INTO magento.core_config_data (scope, scope_id, path, value) VALUES
--- module
-('default', 0, 'mundipagg_config/general_group/module_status', '1'),
-('default', 0, 'mundipagg_config/general_group/logs', '1'),
-('default', 0, 'mundipagg_config/general_group/sk_prod', null),
-('default', 0, 'mundipagg_config/general_group/pk_prod', null),
-('default', 0, 'mundipagg_config/general_group/test_mode', '1'),
-('default', 0, 'mundipagg_config/antifraud_group/antifraud_status', '0'),
-('default', 0, 'mundipagg_config/boleto_group/boleto_status', '1'),
-('default', 0, 'mundipagg_config/boleto_group/boleto_payment_title', 'Boleto'),
-('default', 0, 'mundipagg_config/boleto_group/boleto_name', 'boleto'),
-('default', 0, 'mundipagg_config/boleto_group/boleto_bank', '001'),
-('default', 0, 'mundipagg_config/boleto_group/boleto_due_at', '3'),
-('default', 0, 'mundipagg_config/boleto_group/boleto_instructions', 'Pagar até o vencimento.'),
-('default', 0, 'mundipagg_config/twocreditcards_group/twocreditcards_status', '1'),
-('default', 0, 'mundipagg_config/twocreditcards_group/twocreditcards_payment_title', 'Two credit Cards'),
-('default', 0, 'mundipagg_config/creditcard_group/cards_config_status', '1'),
-('default', 0, 'mundipagg_config/creditcard_group/creditcard_payment_title', 'Credit card'),
-('default', 0, 'mundipagg_config/creditcard_group/invoice_name', 'Magento STG'),
-('default', 0, 'mundipagg_config/creditcard_group/operation_type', 'AuthAndCapture'),
-('default', 0, 'mundipagg_config/creditcard_group/saved_cards_status', '1'),
-('default', 0, 'mundipagg_config/boletocreditcard_group/boleto_cards_config_status', '1'),
-('default', 0, 'mundipagg_config/boletocreditcard_group/boleto_creditcard_payment_title', 'Boleto + Creditcard'),
-('default', 0, 'mundipagg_config/boletocreditcard_group/boleto_cards_name', 'Boleto'),
-('default', 0, 'mundipagg_config/boletocreditcard_group/boleto_cards_bank', '001'),
-('default', 0, 'mundipagg_config/boletocreditcard_group/boleto_cards_due_at', '3'),
-('default', 0, 'mundipagg_config/boletocreditcard_group/boleto_cards_instructions', 'Pague até o venciomento. (CC)'),
-('default', 0, 'mundipagg_config/boletocreditcard_group/boleto_cards_invoice_name', 'Magento STG BoletoCreditCArd'),
-('default', 0, 'mundipagg_config/boletocreditcard_group/boleto_cards_operation_type', 'AuthAndCapture'),
-('default', 0, 'mundipagg_config/installments_group/default_status', '1'),
-('default', 0, 'mundipagg_config/installments_group/default_max_installments', '12'),
-('default', 0, 'mundipagg_config/installments_group/default_max_without_interest', '3'),
-('default', 0, 'mundipagg_config/installments_group/default_interest', '1'),
-('default', 0, 'mundipagg_config/installments_group/default_incremental_interest', '0.5'),
-('default', 0, 'mundipagg_config/installments_group/visa_status', '1'),
-('default', 0, 'mundipagg_config/installments_group/mastercard_status', '1'),
-('default', 0, 'mundipagg_config/installments_group/hipercard_status', '1'),
-('default', 0, 'mundipagg_config/installments_group/diners_status', '1'),
-('default', 0, 'mundipagg_config/installments_group/amex_status', '1'),
-('default', 0, 'mundipagg_config/installments_group/elo_status', '1'),
-('default', 0, 'mundipagg_config/multibuyer_group/multibuyer_status ', '1');
+('default', '0', 'dev/template/allow_symlink', '1'),
+('default', '0', 'payment/mundipagg_debit/active', '1'),
+('default', '0', 'payment/mundipagg_boleto/active', '1'),
+('default', '0', 'payment/mundipagg_creditcard/active', '1'),
+('default', '0', 'payment/mundipagg_standard/display_total', '1'),
+('default', '0', 'payment/mundipagg_recurrencepayment/active', '1'),
+('default', '0', 'payment/mundipagg_standard/enable_installments', '1'),
+('default', '0', 'payment/mundipagg_standard/cc_types', 'VI,MC,AE,DI,EL,HI'),
+('default', '0', 'payment/mundipagg_standard/product_pages_installment_default', NULL),
+('default', '0', 'payment/mundipagg_debit/debit_types', '001,237,341,VBV,cielo_mastercard,cielo_visa'),
+('default', '0', 'payment/mundipagg_standard/installments', 'a:6:{i:0;a:3:{i:0;s:0:\"\";i:1;s:1:\"1\";i:2;s:0:\"\";}i:1;a:3:{i:0;s:0:\"\";i:1;s:1:\"2\";i:2;s:0:\"\";}i:2;a:3:{i:0;s:0:\"\";i:1;s:1:\"3\";i:2;s:0:\"\";}i:3;a:3:{i:0;s:0:\"\";i:1;s:1:\"4\";i:2;s:1:\"1\";}i:4;a:3:{i:0;s:0:\"\";i:1;s:1:\"5\";i:2;s:1:\"2\";}i:5;a:3:{i:0;s:0:\"\";i:1;s:1:\"6\";i:2;s:1:\"3\";}}')
+;
 
+UPDATE magento.core_config_data SET value = '1' where path  = 'payment/mundipagg_twocreditcards/active';
+
+-- magento log
 UPDATE magento.core_config_data SET value = '1' WHERE path = 'dev/log/active';
 UPDATE magento.core_config_data SET value = 'system.log' WHERE path = 'dev/log/file';
 UPDATE magento.core_config_data SET value = 'exception.log' WHERE path = 'dev/log/exception_file';
-
--- creating user
-
-
-
