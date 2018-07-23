4fa8ec733f7e2c723e28890113df8c22cc620976
Ian Lessa <ian.lessa1@gmail.com>
2018-07-23 16:27:16 -0300

    :sparkles: module integrity is now check

--- app/code/community/Uecommerce/Mundipagg/controllers/StandardController.php
+++ app/code/community/Uecommerce/Mundipagg/controllers/StandardController.php
@@ -485,4 +485,122 @@ class Uecommerce_Mundipagg_StandardController extends Mage_Core_Controller_Front
         $payment = $order->getPayment();
         $info = $payment->getAdditionalInformation();
     }
+
+    /** Returns info about module integrity */
+    public function versionAction()
+    {
+        $modmanFilePath = './.modman/magento-module/modman';
+        $integrityCheckFile = './.modman/magento-module/integrityCheck';
+        $integrityData = json_decode(file_get_contents($integrityCheckFile),true);
+
+        $info = [
+            'modmanFilePath' => $modmanFilePath,
+            'integrityCheckFile' => $integrityCheckFile,
+            'php_version' => phpversion()
+        ];
+
+        $modmanRawData = file_get_contents($modmanFilePath);
+
+        $rawLines = explode("\n",$modmanRawData);
+        $lines = [];
+        foreach ($rawLines as $rawLine) {
+            if (
+                substr($rawLine,0,1) === '#' ||
+                strlen($rawLine) === 0
+            ) {
+                continue;
+            }
+            $lines[] = array_values(array_filter(explode(' ',$rawLine)));
+        }
+        foreach ($lines as $index => $line) {
+            $elementName = './' . $line[1];
+            $checkMethod = 'fileCheckSum';
+            if (is_dir($elementName)) {
+                $checkMethod = 'dirCheckSum';
+            }
+            $lines[$index][] = $this->filterFileCheckSum($this->$checkMethod($elementName));
+        }
+        $files = [];
+        foreach($lines as $line) {
+            $files = array_merge($files,end($line));
+        }
+
+        $newFiles = [];
+        $alteredFiles = [];
+        foreach ($files as $fileName => $md5) {
+            if(isset($integrityData[$fileName])) {
+                if ($md5 != $integrityData[$fileName]) {
+                    $alteredFiles[] = $fileName;
+                }
+                continue;
+            }
+            $newFiles[$fileName] = $md5;
+        }
+        $info['newFiles'] = $newFiles;
+        $info['alteredFiles'] = $alteredFiles;
+
+        echo '<pre>';
+        print_r($info);
+        echo '</pre>';
+    }
+
+    protected function dirCheckSum($dir)
+    {
+        $files = scandir($dir);
+        $md5 = [];
+        foreach ($files as $file) {
+            if ($file !== '.' && $file !== '..') {
+                $file = $dir . DIRECTORY_SEPARATOR . $file;
+
+                $checkMethod = 'fileCheckSum';
+                if (is_dir($file)) {
+                    $checkMethod = 'dirCheckSum';
+                }
+
+                $md5[$file] = $this->$checkMethod($file);
+            }
+        }
+        return $md5;
+    }
+
+    protected function filterFileCheckSum($checkSumArray)
+    {
+        $data = serialize($checkSumArray);
+        $data = explode('";s:32:"',$data);
+        $currentFile = null;
+        $currentMd5 = null;
+        $files = [];
+        foreach ($data as $line) {
+            $raw = explode('"',$line);
+            if( $currentFile ) {
+                $files[$currentFile] = $raw[0];
+                $currentFile = null;
+                continue;
+            }
+            $currentFile = end($raw);
+        }
+        return $files;
+    }
+
+    protected function filterFileMd5($pathArray)
+    {
+        $files = [];
+        foreach ($pathArray as $path => $md5) {
+            if (!is_dir($path)) {
+                $files[$path] = $md5;
+            }
+            else {
+                $files = array_merge($files,$this->filterFileMd5($path));
+            }
+        }
+        return $files;
+    }
+
+    protected function fileCheckSum($file)
+    {
+        return  [
+            $file => md5_file($file)
+        ];
+
+    }
 }
